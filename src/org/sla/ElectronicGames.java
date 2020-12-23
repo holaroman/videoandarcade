73d910f9329572067db5edccbdd8dd43859ecf45
holaroman <aroman22@scienceleadership.org>
2020-12-23 10:28:02 -0500

    Initial commit

---
+++ src/org/sla/ElectronicGames.java
@@ -0,0 +1,13 @@
+package org.sla ;
+
+public class ElectronicGames {
+    int ReleaseDate;
+    String GameTitle;
+    int AmountSold;
+
+    public ElectronicGames(int releaseDate, String gameTitle, int amountSold) {
+        ReleaseDate = releaseDate;
+        GameTitle = gameTitle;
+        AmountSold = amountSold;
+    }
+}
